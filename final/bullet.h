e5bfcbc42ab09a603aa341610eee2f8befc7f1ff
tim901231 <b09901020@ntu.edu.tw>
2020-12-29 22:24:36 +0800

    project

--- final/bullet.h
+++ final/bullet.h
@@ -26,36 +26,6 @@ public:
 			quad.w = 90;
 			quad.h = 90;
 		}
-		v_x = (enemy1->rect.x - tower1->x) / speed;
-		v_y = (enemy1->rect.y - tower1->y) / speed;
-		x_e = x;#ifndef BULLET_H
-#define BULLET_H
-#include"tower.h"
-#include"enemy.h"
-#include<cmath>
-class bullet
-{
-public:
-	friend class tower;
-	bullet(tower* tower1, ENEMY* enemy1) {
-		kind = tower1->kind % 3;
-		speed = 100;
-		atk = tower1->atk;
-		x = tower1->x;
-		y = tower1->y;
-		// modify the position of bullet
-		if (kind % 3 == 0) {
-			quad.x = x + 22;
-			quad.y = y + 22;
-			quad.w = 45;
-			quad.h = 45;
-		}
-		else {
-			quad.x = x;
-			quad.y = y;
-			quad.w = 90;
-			quad.h = 90;
-		}
 		v_x = (enemy1->rect.x - tower1->x)*1.0 / speed;
 		v_y = (enemy1->rect.y - tower1->y)*1.0 / speed;
 		x_e = x;
@@ -91,35 +61,4 @@ public:
 	double x_e, y_e, v_x, v_y;
 };
 #endif
-		y_e = y;
-	}
-	void move()
-	{
-		x_e += v_x;
-		y_e += v_y;
-		x = int(x_e);
-		y = int(y_e);
-		if (kind % 3 == 0) {
-			quad.x = x + 22;
-			quad.y = y + 22;
-		}
-		else {
-			quad.x = x;
-			quad.y = y;
-		}
-	}
-	bool touch(ENEMY* enemy1)
-	{
-		//distance unsure
-		if (pow(enemy1->pos.first - x, 2) + pow(enemy1->pos.second - y, 2) < 3600) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	SDL_Rect quad = { 0,0,80,80 };
-	int x, y, speed, atk, kind;
-	double x_e, y_e, v_x, v_y;
-};
-#endif
+		
